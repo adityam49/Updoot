e3ca42fd7b76f91ccc469706150caa39088497a9
Aditya Mahakalkar <adityamahakalkar49@gmail.com>
2019-10-03 20:10:25 +0530

    Converted some util classes to kotlin

---
+++ app/src/main/java/com/ducktapedapps/updoot/utils/Constants.kt
@@ -0,0 +1,40 @@
+package com.ducktapedapps.updoot.utils
+
+object Constants {
+    const val TOKEN_SHARED_PREFS_KEY = "tokenSharedPrefKey"
+    const val USER_TOKEN_REFRESH_KEY = "userTokenRefreshKey"
+
+    const val LOGIN_STATE = "login_state"
+    const val DEVICE_ID_KEY = "deviceIdKey"
+    const val client_id = "jW0kYyiXq1hJyQ"
+    const val redirect_uri = "https://github.com/am-2x49/Updoot"
+    const val API_BASE_URL = "https://oauth.reddit.com/"
+    const val TOKEN_ACCESS_URL = "https://www.reddit.com/api/v1/access_token"
+    const val scopes = "identity,edit,flair,history,modconfig,modflair,modlog,modposts,modwiki,mysubreddits,privatemessages,read,report,save,submit,subscribe,vote,wikiedit,wikiread"
+    const val userLess_grantType = "https://oauth.reddit.com/grants/installed_client"
+    const val user_grantType = "authorization_code"
+    const val user_refresh_grantType = "refresh_token"
+
+    //submissions sorts
+    const val HOT = "hot"
+    const val TOP = "top"
+    const val NEW = "new"
+    const val RISING = "rising"
+    const val BEST = "best"
+    const val CONTROVERSIAL = "controversial"
+
+    //submissions sort time
+    const val NOW = "hour"
+    const val TODAY = "day"
+    const val THIS_WEEK = "week"
+    const val THIS_MONTH = "month"
+    const val THIS_YEAR = "year"
+    const val ALL_TIME = "all"
+
+    const val ACCOUNT_LOGIN_REQUEST_CODE = 99
+
+    const val ACCOUNT_TYPE = "updoot_account"
+    const val ANON_USER = "Anonymous"
+
+    const val DIFF_VOTE_KEY = "diff_vote_key"
+}
