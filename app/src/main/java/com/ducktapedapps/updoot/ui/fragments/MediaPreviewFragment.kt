9e41b4b528bb3ca2ef82943130bc19743daa7a95
Aditya Mahakalkar <adityamahakalkar49@gmail.com>
2019-12-09 15:37:20 +0530

    SafeArgs use navArgs() property delegates

--- app/src/main/java/com/ducktapedapps/updoot/ui/fragments/MediaPreviewFragment.kt
+++ app/src/main/java/com/ducktapedapps/updoot/ui/fragments/MediaPreviewFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.ducktapedapps.updoot.R
 import com.github.chrisbanes.photoview.PhotoView
@@ -13,13 +14,13 @@ import com.github.chrisbanes.photoview.PhotoView
 const val TAG = "MediaPreviewFragment"
 
 class MediaPreviewFragment : DialogFragment() {
+
+    private val args: MediaPreviewFragmentArgs by navArgs()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.image_preview_fragment, container, false)
         val photoView: PhotoView = root.findViewById(R.id.imageView)
-        arguments?.run {
-            val args: MediaPreviewFragmentArgs = MediaPreviewFragmentArgs.fromBundle(this)
-            Glide.with(photoView.context).load(args.mediaUrl).into(photoView)
-        }
+        Glide.with(photoView.context).load(args.mediaUrl).into(photoView)
         return root
     }
 
