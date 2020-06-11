cb5b2a8da4c3797d4b259bafaa88f9e2479e365f
Aditya Mahakalkar <adityamahakalkar49@gmail.com>
2020-06-11 23:31:27 +0530

    Add constraint setRequiresDeviceIdle to cache cleanup worker (api 23+)

--- app/src/main/java/com/ducktapedapps/updoot/backgroundWork/cacheCleanUp/EnqueueCleanUpWork.kt
+++ app/src/main/java/com/ducktapedapps/updoot/backgroundWork/cacheCleanUp/EnqueueCleanUpWork.kt
@@ -1,6 +1,7 @@
 package com.ducktapedapps.updoot.backgroundWork.cacheCleanUp
 
 import android.content.Context
+import android.os.Build
 import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
@@ -12,11 +13,12 @@ private const val CACHED_SUBMISSIONS_CLEANUP_WORKER_TAG = "cached_submissions_cl
 fun enqueueCleanUpWork(context: Context) {
     val constraints = Constraints.Builder()
             .setRequiresBatteryNotLow(true)
-            .build()
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) constraints.setRequiresDeviceIdle(true)
 
     val cleanUpWorkRequest = PeriodicWorkRequestBuilder<SubmissionsCacheCleanUpWorker>(3, TimeUnit.HOURS)
             .addTag(CACHED_SUBMISSIONS_CLEANUP_WORKER_TAG)
-            .setConstraints(constraints)
+            .setConstraints(constraints.build())
             .build()
 
     WorkManager.getInstance(context).enqueueUniquePeriodicWork(
