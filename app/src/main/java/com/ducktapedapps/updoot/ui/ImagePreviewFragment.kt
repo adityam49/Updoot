ea29cb4dbcaf869092c9ff9d2ba8973ce0c6f3df
Aditya Mahakalkar <adityamahakalkar49@gmail.com>
2020-07-29 19:06:00 +0530

    Add scrim to Image Preview

--- app/src/main/java/com/ducktapedapps/updoot/ui/ImagePreviewFragment.kt
+++ app/src/main/java/com/ducktapedapps/updoot/ui/ImagePreviewFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.ducktapedapps.updoot.R
 import com.ducktapedapps.updoot.databinding.FragmentImagePreviewBinding
 
 
@@ -28,6 +30,20 @@ class ImagePreviewFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentImagePreviewBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        activity?.window?.apply {
+            statusBarColor = ContextCompat.getColor(requireContext(), R.color.color_scrim)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        activity?.window?.apply {
+            statusBarColor = ContextCompat.getColor(requireContext(), R.color.color_primary_variant)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
