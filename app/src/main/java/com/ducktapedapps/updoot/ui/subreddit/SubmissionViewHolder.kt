f439166c90d0b0f09825a8a983a6db065713f8f2
Aditya Mahakalkar <adityamahakalkar49@gmail.com>
2020-09-12 22:01:39 +0530

    Large submission ui refactor

--- app/src/main/java/com/ducktapedapps/updoot/ui/subreddit/SubmissionViewHolder.kt
+++ app/src/main/java/com/ducktapedapps/updoot/ui/subreddit/SubmissionViewHolder.kt
@@ -11,6 +11,8 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.TransitionManager
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import com.ducktapedapps.updoot.R
 import com.ducktapedapps.updoot.databinding.CompactSubmissionImageBinding
@@ -138,16 +140,17 @@ sealed class SubmissionViewHolder(itemView: View) : RecyclerView.ViewHolder(item
                         true
                     }
                     previewImageView.apply {
-                        setOnClickListener { clickHandler.performAction(this@with) }
-                        imageSet?.lowResUrl?.let {
+                        if (imageSet?.lowResUrl != null)
                             Glide.with(this)
-                                    .load(it)
-                                    .thumbnail(Glide.with(previewImageView).load(thumbnail))
-                                    .placeholder(R.color.color_on_surface)
+                                    .load(imageSet.lowResUrl)
+                                    .fitCenter()
+                                    .transform(RoundedCorners(8))
+                                    .placeholder(R.color.color_surface)
                                     .error(R.drawable.ic_image_error_24dp)
-                                    .into(this)
-
-                        }
+                                    .transition(DrawableTransitionOptions.withCrossFade())
+                                    .into(this).also { visibility = View.VISIBLE }
+                        else visibility = View.GONE
+                        setOnClickListener { clickHandler.performAction(this@with) }
                     }
                     setGildings(gildingTextView, gildings)
                     scoreView.setData(ups, likes)
