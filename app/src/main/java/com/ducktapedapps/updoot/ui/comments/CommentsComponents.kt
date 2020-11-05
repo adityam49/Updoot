4746732dc923a012047ed82642c67dabc6f46a0c
Aditya Mahakalkar <adityamahakalkar49@gmail.com>
2020-11-05 17:13:06 +0530

    Add load more comments nodes to comment tree and gildings.

--- app/src/main/java/com/ducktapedapps/updoot/ui/comments/CommentsComponents.kt
+++ app/src/main/java/com/ducktapedapps/updoot/ui/comments/CommentsComponents.kt
@@ -1,24 +1,32 @@
 package com.ducktapedapps.updoot.ui.comments
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
+import com.ducktapedapps.updoot.R
 import com.ducktapedapps.updoot.data.local.model.Comment.CommentData
+import com.ducktapedapps.updoot.data.local.model.Comment.MoreCommentData
 import com.ducktapedapps.updoot.data.local.model.Gildings
 import com.ducktapedapps.updoot.data.local.model.Listing
 import com.ducktapedapps.updoot.ui.theme.*
+import com.ducktapedapps.updoot.utils.getCompactCountAsString
 
 
 private val commentData = CommentData(
@@ -65,19 +73,17 @@ private fun CommentBody(text: String, modifier: Modifier) {
 
 @Composable
 private fun CommentHeader(
-        authorName: String,
-        replies: Int,
-        ups: Int?,
-        likes: Boolean?,
-        isOp: Boolean,
-        isExpanded: Boolean,
+        commentData: CommentData,
         modifier: Modifier
 ) {
     Row(modifier = modifier.fillMaxWidth().wrapContentHeight().padding(4.dp)) {
-        UserName(username = authorName, isOp = isOp)
-        Spacer(modifier = Modifier.weight(1f))
-        if (replies != 0 && !isExpanded) ReplyCounter(replyCount = replies)
-        VoteCounter(ups = ups, likes = likes)
+        with(commentData) {
+            UserName(username = author, isOp = is_submitter)
+            Spacer(modifier = Modifier.weight(1f))
+            AllGildings(gildings = gildings)
+            if (replies.children.isNotEmpty() && !repliesExpanded) ReplyCounter(replyCount = replies.children.size)
+            VoteCounter(ups = ups, likes = likes)
+        }
     }
 }
 
@@ -102,7 +108,7 @@ private fun ReplyCounter(replyCount: Int) {
     ColoredTag(color = background) {
         Text(
                 color = MaterialTheme.colors.ColorOnScoreBackground,
-                text = "+ $replyCount",
+                text = "+ ${getCompactCountAsString(replyCount.toLong())}",
                 style = MaterialTheme.typography.overline,
                 modifier = Modifier.padding(start = 4.dp, end = 4.dp)
         )
@@ -112,7 +118,7 @@ private fun ReplyCounter(replyCount: Int) {
 @Composable
 private fun VoteCounter(ups: Int?, likes: Boolean?) {
     Text(
-            text = ups?.toString() ?: "?",
+            text = ups?.toLong()?.run { getCompactCountAsString(this) } ?: "?",
             style = MaterialTheme.typography.overline,
             color = when (likes) {
                 true -> upVoteColor
@@ -185,12 +191,7 @@ fun FullComment(
                 })
 
         CommentHeader(
-                authorName = comment.author,
-                replies = comment.replies.children.size,
-                ups = comment.ups,
-                likes = comment.likes,
-                isOp = comment.is_submitter,
-                isExpanded = comment.repliesExpanded,
+                commentData = comment,
                 modifier = Modifier.constrainAs(header) {
                     start.linkTo(indentation.end)
                     top.linkTo(parent.top)
@@ -213,6 +214,65 @@ fun FullComment(
 }
 
 @Composable
+fun MoreComment(
+        data: MoreCommentData,
+        loadMoreComments: () -> Unit,
+        singleThreadMode: Boolean,
+        threadSpacingWidth: Dp,
+        threadWidth: Dp,
+) {
+    ConstraintLayout {
+        val (indentation, body) = createRefs()
+        IndentationThread(
+                modifier = Modifier.constrainAs(indentation) {
+                    height = Dimension.fillToConstraints
+                    width = Dimension.wrapContent
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                    end.linkTo(body.start)
+                    bottom.linkTo(parent.bottom)
+                },
+                singleThreadMode = singleThreadMode,
+                indentLevel = data.depth,
+                threadWidth = threadWidth,
+                spacingWidth = threadSpacingWidth,
+        )
+        OutlinedButton(
+                onClick = loadMoreComments,
+                modifier = Modifier.constrainAs(body) {
+                    start.linkTo(indentation.end, margin = 8.dp)
+                    top.linkTo(parent.top, margin = 4.dp)
+                    bottom.linkTo(parent.bottom, margin = 4.dp)
+                    end.linkTo(parent.end)
+                    width = Dimension.wrapContent
+                    height = Dimension.wrapContent
+                }
+        ) {
+            Text(
+                    text = "Load ${data.children.size} comments",
+                    style = MaterialTheme.typography.body2
+            )
+        }
+
+    }
+}
+
+@Composable
+private fun AllGildings(gildings: Gildings) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        if (gildings.silver != 0) SingleGilding(resId = R.drawable.ic_silver_gilding_14dp, count = gildings.silver)
+        if (gildings.gold != 0) SingleGilding(resId = R.drawable.ic_gold_gilding_14dp, count = gildings.gold)
+        if (gildings.platinum != 0) SingleGilding(resId = R.drawable.ic_platinum_gilding_14dp, count = gildings.platinum)
+    }
+}
+
+@Composable
+fun SingleGilding(@DrawableRes resId: Int, count: Int) {
+    Text(text = count.toString(), style = MaterialTheme.typography.overline)
+    Image(asset = vectorResource(id = resId))
+}
+
+@Composable
 private fun IndentationThread(
         modifier: Modifier,
         singleThreadMode: Boolean,
@@ -224,13 +284,13 @@ private fun IndentationThread(
     val isLight = MaterialTheme.colors.isLight
     Canvas(modifier = modifier.width(((spacingWidth.value + threadWidth.value) * indentLevel).dp), onDraw = {
         if (singleThreadMode) drawLine(
-                color = if (isLight) LightThreadColors[indentLevel] else DarkThreadColors[indentLevel],
+                color = if (isLight) LightThreadColors[indentLevel - 1] else DarkThreadColors[indentLevel - 1],
                 start = Offset((spacingWidth.toPx() + threadWidth.toPx()) * indentLevel, 0f),
                 end = Offset((spacingWidth.toPx() + threadWidth.toPx()) * indentLevel, drawContext.size.height),
                 strokeWidth = threadWidth.toPx(),
         )
         else for (thread in 1..indentLevel) drawLine(
-                color = if (isLight) LightThreadColors[thread] else DarkThreadColors[thread],
+                color = if (isLight) LightThreadColors[thread - 1] else DarkThreadColors[thread - 1],
                 start = Offset(x = thread * (spacingWidth.toPx() + threadWidth.toPx()), 0f),
                 end = Offset(x = thread * (spacingWidth.toPx() + threadWidth.toPx()), drawContext.size.height),
                 strokeWidth = threadWidth.toPx(),
--- app/src/main/java/com/ducktapedapps/updoot/ui/comments/CommentsScreen.kt
+++ app/src/main/java/com/ducktapedapps/updoot/ui/comments/CommentsScreen.kt
@@ -1,43 +1,61 @@
 package com.ducktapedapps.updoot.ui.comments
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumnForIndexed
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.asFlow
 import com.ducktapedapps.updoot.data.local.model.Comment
+import com.ducktapedapps.updoot.data.local.model.Comment.CommentData
+import com.ducktapedapps.updoot.data.local.model.Comment.MoreCommentData
 import kotlinx.coroutines.flow.Flow
 
 @Composable
-fun CommentsScreen(
-        viewModel: CommentsVM
-) {
-    if (viewModel.isLoading.asFlow().collectAsState(initial = true).value)
-        CircularProgressIndicator(modifier = Modifier.size(48.dp))
-    else
+fun CommentsScreen(viewModel: CommentsVM) {
+    Box {
+        if (viewModel.isLoading.asFlow().collectAsState(initial = true).value)
+            CircularProgressIndicator(
+                    modifier = Modifier.size(48.dp).align(Alignment.Center)
+            )
         AllComments(
                 allComments = viewModel.allComments,
-                loadMoreComments = viewModel::toggleChildrenVisibility,
+                toggleChildrenVisibility = viewModel::toggleChildrenVisibility,
+                loadMoreComments = viewModel::loadMoreComment
         )
+    }
 }
 
 
 @Composable
-fun AllComments(allComments: Flow<List<Comment>>, loadMoreComments: (Int) -> Unit) {
+fun AllComments(
+        allComments: Flow<List<Comment>>,
+        toggleChildrenVisibility: (Int) -> Unit,
+        loadMoreComments: (MoreCommentData, Int) -> Unit
+) {
     LazyColumnForIndexed(
             items = allComments.collectAsState(initial = emptyList()).value
     ) { index: Int, comment: Comment ->
-        if (comment is Comment.CommentData)
-            FullComment(
+        when (comment) {
+            is CommentData -> FullComment(
                     comment = comment,
-                    onClickComment = { loadMoreComments(index) },
-                    singleThreadMode = true, // TODO : get value from preference storage
+                    onClickComment = { toggleChildrenVisibility(index) },
+                    singleThreadMode = false, // TODO : get value from preference storage
                     threadSpacingWidth = 6.dp,
                     threadWidth = 2.dp,
             )
+            is MoreCommentData -> MoreComment(
+                    data = comment,
+                    loadMoreComments = { loadMoreComments(comment, index) },
+                    singleThreadMode = false, // TODO : get value from preference storage
+                    threadSpacingWidth = 6.dp,
+                    threadWidth = 2.dp,
+            )
+        }
     }
 }
 
